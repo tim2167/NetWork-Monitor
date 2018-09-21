9ba5042db366e46542cf099fa62f16cfb1ea8d15
Tim Chang <tinghaur@umail.ucsb.edu>
2018-09-22 03:53:14 +0800

    Transition from local folder to github.

---
+++ Backup/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Lucida Console", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(93, 16)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(433, 222)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(90, 241)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(548, 293)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿'----------------------------------------------------------------------------------------------------------------------------
+'Script Name : BandwidthMonitor.sln
+'Author      : Andrew Samuel
+'Created     : 16/09/2010
+'Description : Tracks the inbound and outbound traffic on a PC
+'----------------------------------------------------------------------------------------------------------------------------
+'We need to import System.Math to allow a bit of rounding
+'----------------------------------------------------------------------------------------------------------------------------
+Imports System.Math
+
+Public Class Form1
+    '----------------------------------------------------------------------------------------------------------------------------
+    'Here we declare all our variables
+    '----------------------------------------------------------------------------------------------------------------------------
+    Dim objProcess As New Process               'Used to process the netstat command
+    Dim strOutput As String                     'A string to store the output of the netstat process
+    Dim strError As String                      'A string to store any errors of the netstat process
+    Dim DLoad As Long                           'This will be the raw download data from netstat
+    Dim ULoad As Long                           'This will be the raw upload data from netstat
+    Dim LastULoad As Long = 0                   'This will be used to calculate the surrent usage
+    Dim LastDLoad As Long = 0                   'This will be used to calculate the current usage
+    Dim SString As String()                     'We need this string array to give us the ability to get the data we need from the netstat command
+    Dim doAfterSecondPass As Boolean = False    'This we will use to allow the program to process something on and after the second pass through
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Start()                          'This will start polling the netstat each second
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        'This is where the fun starts!
+        '
+        'We need to set all the variables inside the objProcess
+        objProcess.StartInfo.RedirectStandardOutput = True
+        objProcess.StartInfo.CreateNoWindow = True
+        objProcess.StartInfo.RedirectStandardError = True
+        objProcess.StartInfo.FileName() = "netstat"
+        objProcess.StartInfo.Arguments() = "-e"
+        objProcess.StartInfo.UseShellExecute = False
+        'Now we will start the process
+        objProcess.Start()
+        'Here we will store the outputs to our 2 strings
+        strOutput = objProcess.StandardOutput.ReadToEnd
+        strError = objProcess.StandardError.ReadToEnd
+        'And then we wait for the process to end before carrying on
+        objProcess.WaitForExit()
+        'Now we output the information we have
+        TextBox1.Text = strOutput 'This is the raw data from netstat -e
+        SString = Split(strOutput, "Bytes") 'We split that string at the word Bytes
+        DLoad = Int(Mid(SString(1), 14, 17)) 'Find the download figure and store it to DLoad variable
+        ULoad = Int(Mid(SString(1), 31, 16)) 'Find the upload figure and store it to ULoad variable
+
+        'Here is where we work out how much data has passed in the last second
+        If doAfterSecondPass = True Then
+            Label1.Text = "Upload: " & roundBytes(ULoad - LastULoad) & "/s" & vbCrLf & "Download: " & roundBytes(DLoad - LastDLoad) & "/s"
+        Else
+            doAfterSecondPass = True
+        End If
+        'And this is where we set the las values ready to use next time
+        LastDLoad = DLoad
+        LastULoad = ULoad
+    End Sub
+
+    '----------------------------------------------------------------------------------------------------------------------------
+    'This is the function to return a slightly friendlier value for the bytes we got from netstat
+    '----------------------------------------------------------------------------------------------------------------------------
+    Function roundBytes(ByVal bytes As String)
+        If bytes > 1073741824 Then
+            Return Round(bytes / 1073741824, 2) & " GB"
+        ElseIf bytes > 1048576 Then
+            Return Round(bytes / 1048576, 2) & " MB"
+        ElseIf bytes > 1024 Then
+            Return Round(bytes / 1024, 2) & " KB"
+        ElseIf bytes = 0 Then
+            Return bytes & " B"
+        Else
+            Return bytes & " B"
+        End If
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    End Sub
+End Class
